028482f0d41fea84e3d730825b46e85229f3d8f1
Danil Skachkov <danil.skachkov@gmail.com>
2017-12-27 16:44:51 -0800

    Initial commit

---
+++ spec/font-storage-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FontStorageView from '../lib/font-storage-view';
+
+describe('FontStorageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
