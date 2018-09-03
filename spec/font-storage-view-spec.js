c4312aa68e09dbd3551ada36a3636006f73ae0f2
Danil Skachkov <danil.skachkov@gmail.com>
2018-09-02 18:54:07 -0700

    Renamings

--- spec/font-storage-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import FontStorageView from '../lib/font-storage-view';
-
-describe('FontStorageView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
